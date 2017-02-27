d129d2c428eca786aa3857b3ffc7dff6f823c267
Andrea Bigagli <abigagli@gmail.com>
2017-02-27 12:28:07 +0100

    Updated environment for my preferred setup

--- vars.sh
+++ vars.sh
@@ -1,17 +1,32 @@
 #! /bin/bash
-INSTALL_PATH=/opt/cross
+FACTORY_ROOT=/Volumes/GNU_FACTORY/AUTOMATIC
+INSTALL_PATH=$FACTORY_ROOT/INSTALL
+TARBALLS_PATH=$FACTORY_ROOT/TARBALLS
 TARGET=x86_64-linux
 USE_NEWLIB=0
 LINUX_ARCH=x86_64
-CONFIGURATION_OPTIONS="--disable-multilib --disable-nls" # --disable-threads --disable-shared
+CONFIGURATION_OPTIONS="--disable-multilib --disable-nls --disable-werror" # --disable-threads --disable-shared
 PARALLEL_MAKE=-j4
-BINUTILS_VERSION=binutils-2.25
-GCC_VERSION=gcc-4.9.2
-LINUX_KERNEL_VERSION=linux-3.17.2
-GLIBC_VERSION=glibc-2.20
-MPFR_VERSION=mpfr-3.1.2
-GMP_VERSION=gmp-6.0.0a
-MPC_VERSION=mpc-1.0.2
-ISL_VERSION=isl-0.12.2
-CLOOG_VERSION=cloog-0.18.1
+BINUTILS_VERSION=binutils-2.27
+GCC_VERSION=gcc-6.3.0
+LINUX_KERNEL_VERSION=linux-3.19
+GLIBC_VERSION=glibc-2.25
+MPFR_VERSION=mpfr-3.1.5
+GMP_VERSION=gmp-6.1.2
+MPC_VERSION=mpc-1.0.3
+ISL_VERSION=isl-0.16.1
+CLOOG_VERSION=cloog-0.18.4
+
+readlink ()
+{
+    greadlink "$@"
+}
+export -f readlink
+
+sed ()
+{
+    gsed "$@"
+}
+export -f sed
+
 export PATH=$INSTALL_PATH/bin:$PATH
